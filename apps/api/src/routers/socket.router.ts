de2c1caa6f04559192ed514461607f0e13d89c78
Ronit <pandaronit25@gmail.com>
2024-05-27 18:13:33 +0530

    docs: api route doc

--- apps/api/src/routers/socket.router.ts
+++ apps/api/src/routers/socket.router.ts
@@ -6,6 +6,6 @@ import { authenticate } from "../middlewares/auth.middleware";
 export const socketRouter = Router({ mergeParams: true });
 
 /**
- * @method GET @url /socket/token @desc get's the current authenticated user
+ * @method GET @url /socket/token @desc get's the ably token request for the user
  */
 socketRouter.get("/token", authenticate, socketController.getToken);
