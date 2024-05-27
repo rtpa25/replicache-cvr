ad098e4eb88870d5abc572c0ca34cf581ab85aed
Ronit <pandaronit25@gmail.com>
2024-05-27 18:12:25 +0530

    fix: ably auth fix

--- apps/api/src/controllers/socket.controller.ts
+++ apps/api/src/controllers/socket.controller.ts
@@ -1,13 +1,12 @@
 import { type NextFunction, type Request, type RequestHandler, type Response } from "express";
-import { nanoid } from "nanoid";
 
 import { ably } from "@repo/lib";
 
 class SocketController {
-  public getToken: RequestHandler = async (_req: Request, res: Response, next: NextFunction) => {
+  public getToken: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const tokenRes = await ably.auth.createTokenRequest({
-        clientId: nanoid(),
+        clientId: req.user.id,
       });
 
       res.status(200).json(tokenRes);
